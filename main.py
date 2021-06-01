68bdbba2859b861bb7cc51e3e33e54722b0502fe
saiko-2043 <bessapachecolr@gmail.com>
2021-06-01 11:56:14 +0100

    re added normal .append to plotting lists. Previous change was for debugging

--- main.py
+++ main.py
@@ -4,9 +4,12 @@ By: Saiko2043
 """
 import requests
 import matplotlib.pyplot as plt
+from matplotlib import style
 import random
 from itertools import count
 from matplotlib.animation import FuncAnimation
+
+url = "https://api.wheretheiss.at/v1/satellites/25544"
 
 y_velocity = []
 y_altitude = []
@@ -21,24 +24,24 @@ fig = plt.figure(figsize=(15,15))
 
 fig.suptitle("ISS TRACKER")
 
-ax1 = fig.add_subplot(221)
-ax2 = fig.add_subplot(222, sharex= ax1)
-ax3 = fig.add_subplot(223, sharex= ax1)
-ax4 = fig.add_subplot(224, sharex=ax1)
+ax1 = fig.add_subplot(2, 2, 1)
+ax2 = fig.add_subplot(2, 2, 2, sharex= ax1)
+ax3 = fig.add_subplot(2, 2, 3, sharex= ax1)
+ax4 = fig.add_subplot(2, 2, 4, sharex=ax1)
+
 
 ax1.set_ylabel('Velocity')
 ax2.set_ylabel('Altitude')
 ax3.set_ylabel('Latitude')
 ax4.set_ylabel('Longitude')
 
-ax1.ticklabel_format(useOffset=False, style='plain')
+""" ax1.ticklabel_format(useOffset=False, style='plain')
 ax2.ticklabel_format(useOffset=False, style='plain')
 ax3.ticklabel_format(useOffset=False, style='plain')
-ax4.ticklabel_format(useOffset=False, style='plain')
+ax4.ticklabel_format(useOffset=False, style='plain') """
 
 def updateData(i):
     
-    url = "https://api.wheretheiss.at/v1/satellites/25544"
     data = requests.get(url).json()
  
     
@@ -47,18 +50,19 @@ def updateData(i):
     latitude = data['latitude']
     longitude = data['longitude']
 
-
-    y_velocity.append(random.randrange(10))
-    y_altitude.append(random.randrange(10))
-    y_latitude.append(random.randrange(10))
-    y_longitude.append(random.randrange(10))
+    
+    y_velocity.append(velocity)
+    y_altitude.append(altitude)
+    y_latitude.append(latitude)
+    y_longitude.append(longitude)
 
     x_delta_time.append(next(index))
+
 
     ax1.plot(x_delta_time, y_velocity, color='red')
     ax2.plot(x_delta_time, y_altitude, color='blue')
     ax3.plot(x_delta_time, y_latitude, color='orange')
     ax4.plot(x_delta_time, y_longitude, color='green')
 
-animate = FuncAnimation(fig, updateData, interval = 1)
+animate = FuncAnimation(fig, updateData, interval = 1000)
 plt.show()
